e5e341ce006d3343c131feadba230c8148145103
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-10-21 21:31:45 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Canvas} from "@react-three/fiber";
+
+function App() {
+  return (
+      <div id={"canvas-container"}>
+          <Canvas>
+              <mesh>
+                  <boxGeometry />
+                  <meshStandardMaterial />
+              </mesh>
+          </Canvas>
+      </div>
+  )
+}
+
+export default App
