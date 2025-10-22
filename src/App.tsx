0aae8cc51cf8dcb64db80f2a6256ae6e31dd4337
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-10-22 00:22:46 -0400

    Add pig

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import {Canvas} from "@react-three/fiber";
+import {Pig} from "./Pig.tsx";
+import {CameraControls, Environment, Plane} from "@react-three/drei";
 
 function App() {
   return (
       <div id={"canvas-container"}>
           <Canvas>
-              <mesh>
-                  <boxGeometry />
-                  <meshStandardMaterial />
-              </mesh>
+              <CameraControls/>
+              <Pig position={[0,-1,0]}/>
+              <Environment files={"studio_small_02_1k.exr"}/>
+              <Plane args={[10,10]} rotation={[-1.5,0,0]} position={[0,-1.6,0]}>
+                  <meshStandardMaterial color={[0,0.25,0]}/>
+              </Plane>
           </Canvas>
       </div>
   )
