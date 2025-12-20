27791b6e2d7bec680cce698f9ce3ed8fdf283cb8
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-12-19 23:22:07 -0500

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div id={"canvas-container"}>
           <Canvas>
               <CameraControls/>
-              <Pig position={[0,-1,0]}/>
+              <Pig position={[0,-1,0]} rotation={[0,0.5,0]}/>
               <Environment files={"studio_small_02_1k.exr"}/>
               <Plane args={[10,10]} rotation={[-1.5,0,0]} position={[0,-1.6,0]}>
                   <meshStandardMaterial color={[0,0.25,0]}/>
