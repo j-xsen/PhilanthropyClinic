9b8eb0b9ee7c8c99c21e991e9a320fd2aca60447
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-12-20 22:46:43 -0500

    jumping pig

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Canvas} from "@react-three/fiber";
 import {Pig} from "./Pig.tsx";
 import {CameraControls, Environment, Plane} from "@react-three/drei";
+import {Carrot} from "./Carrot.tsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Canvas>
               <CameraControls/>
               <Pig position={[0,-1,0]} rotation={[0,0.5,0]}/>
+              <Carrot position={[0,0,0]}/>
               <Environment files={"studio_small_02_1k.exr"}/>
               <Plane args={[10,10]} rotation={[-1.5,0,0]} position={[0,-1.6,0]}>
                   <meshStandardMaterial color={[0,0.25,0]}/>
