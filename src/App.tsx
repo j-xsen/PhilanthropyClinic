4691da9f578b2b0dde15bc348852c382e7e26930
Jaxsen Honeycutt <jaxsen@jxsen.com>
2025-12-24 01:33:07 -0500

    add jail

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import {Canvas} from "@react-three/fiber";
+import {Canvas, useLoader} from "@react-three/fiber";
 import {PiggyBank} from "./PiggyBank.tsx";
 import {DragControls, Environment, Plane} from "@react-three/drei";
 import {Carrot} from "./Carrot.tsx";
-import {Matrix4} from "three";
+import {Matrix4, TextureLoader} from "three";
 import {Suspense, useRef, useState} from "react";
 import {type CollisionPayload, Physics, RapierRigidBody, RigidBody, vec3} from "@react-three/rapier";
+import {Jail} from "./Jail.tsx";
 
 function App() {
     const check_carrot_on_pig = (_localMatrix: Matrix4,
@@ -35,6 +36,9 @@ function App() {
     const piggyRigidRef = useRef<RapierRigidBody>(null!);
     const piggyRef = useRef(null!);
     const carrotRef = useRef<RapierRigidBody>(null!);
+
+    const grassColorMap = useLoader(TextureLoader, 'textures/grass-color.avif');
+
     return (
         <div id={"canvas-container"}>
             <Canvas>
@@ -43,6 +47,7 @@ function App() {
                         <RigidBody colliders={"cuboid"} name={"piggy"} ref={piggyRigidRef} onIntersectionEnter={pig_collide} onIntersectionExit={pig_end_collide}>
                             <PiggyBank ref={piggyRef} curAnim={curAnim} position={[0, -2, -4]} rotation={[0, 0.5, 0]}/>
                         </RigidBody>
+                        <Jail position={[0,0,-4]}/>
                         <DragControls autoTransform={false} axisLock="z" onDrag={check_carrot_on_pig}>
                             <RigidBody name={"carrot"} position={[0, 0, -4]} colliders={"cuboid"} gravityScale={0} ref={carrotRef} sensor>
                                 <Carrot/>
@@ -50,7 +55,7 @@ function App() {
                         </DragControls>
                         <RigidBody name="plane" colliders={"cuboid"} restitution={1} >
                             <Plane args={[20, 20]} rotation={[-1.5, 0, 0]} position={[0, -4, -4]}>
-                                <meshStandardMaterial color={[0, 0.25, 0]}/>
+                                <meshStandardMaterial color={[0, 0.25, 0]} map={grassColorMap}/>
                             </Plane>
                         </RigidBody>
                     </Physics>
