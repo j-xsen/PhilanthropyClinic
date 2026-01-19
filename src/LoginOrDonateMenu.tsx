b88c611eb01c9bdc2215a3c009e7b85e0be4d1c2
Jaxsen Honeycutt <jaxsen@jxsen.com>
2026-01-19 14:11:26 -0500

    move HTML form into ui container

--- src/LoginOrDonateMenu.tsx
+++ src/LoginOrDonateMenu.tsx
@@ -1,5 +1,5 @@
 import {GoldPlate} from "./GoldPlate.tsx";
-import {Container, Text} from "@react-three/uikit";
+import {Container, Content, Text} from "@react-three/uikit";
 import {useEffect, useState} from "react";
 import {Html} from "@react-three/drei";
 
@@ -35,20 +35,22 @@ export default function LoginOrDonateMenu(props:{panelState:boolean, setPanelSta
     }
 
     return <>
-    <group position={[0, 0, 3]}>
-        <GoldPlate position={[0,0,-0.2]}/>
+        <group position={[0,0,2.9]}>
+        <GoldPlate/>
+        </group>
+    <group position={[0, 0, 3.1]}>
         <Container width={132} marginTop={-3.5}>
             <Container marginRight={3}>
             <Text color={"#ddd"} fontSize={6} marginRight={2} marginLeft={2}>EMAIL:</Text>
-            </Container>
-        </Container>
-        <group>
+                <Content>
                 <Html>
-                    <form style={{width:"100%", marginLeft:"-51%", marginTop:"-2rem"}}>
-                    <input style={{width:200, backgroundColor: "rgba(68,68,68,0.2)", border: "hidden", fontSize:23, padding:"5px", color:"#fff"}} name={"email"} type={"email"} placeholder={"email@email.com"}></input>
+                    <form style={{width:"100%", marginTop:-5}}>
+                        <input style={{width:175, backgroundColor: "rgba(68,68,68,0.2)", border: "hidden", fontSize:18, padding:"5px", color:"#fff"}} name={"email"} type={"email"} placeholder={"email@email.com"}></input>
                     </form>
                 </Html>
-        </group>
+                </Content>
+            </Container>
+        </Container>
         <group position={[.55,-.05,0.2]}>
         <Container backgroundColor={exitColor} width={10} height={10} positionType={"absolute"} positionTop={-65} onPointerEnter={exitMouseEnter} onPointerLeave={exitMouseExit} onPointerDown={exitMouseDown} onPointerUp={exitMouseExit} onClick={exitFunc}>
             <Text fontWeight={"bold"} marginLeft={1.5} color={"white"} fontSize={9}>X</Text>
