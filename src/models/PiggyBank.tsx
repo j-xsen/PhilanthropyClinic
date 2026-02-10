0791cf893b57a108a5ff3f9ac09917b866b475b6
Jaxsen Honeycutt <j0hone01@louisville.edu>
2026-02-09 21:34:34 -0500

    add texture to Piggy

--- src/models/PiggyBank.tsx
+++ src/models/PiggyBank.tsx
@@ -1,20 +1,73 @@
 import {useEffect, useMemo} from 'react'
-import { useGLTF, useAnimations } from '@react-three/drei'
-import { SkeletonUtils } from 'three-stdlib'
-import type {JSX} from "react/jsx-runtime";
+import {useGLTF, useAnimations, useKTX2} from '@react-three/drei'
+import {SkeletonUtils} from 'three-stdlib'
+import type {JSX} from "react/jsx-runtime"
+import {Material, Mesh, MeshPhysicalMaterial, MeshStandardMaterial, SRGBColorSpace} from "three"
 
 type PiggyBankProps = JSX.IntrinsicElements['group'] & {
-  curAnim: "Breathe" | "Jump";
+  curAnim: "Breathe" | "Jump"
 }
 
 export function PiggyBank({curAnim, ...props}: PiggyBankProps) {
   const { scene, animations } = useGLTF('/pig-transformed.glb')
 
-  // Clone preserves skeleton + animation bindings
   const clone = useMemo(
       () => SkeletonUtils.clone(scene),
       [scene]
   )
+
+  const [pig, eye, pupil] = useKTX2([
+    '/textures/pig/base.ktx2',
+    '/textures/pig/eye.ktx2',
+    '/textures/pig/pupil.ktx2'
+  ])
+
+  useEffect(() => {
+    if (!pig && !eye && !pupil) return
+
+    clone.traverse((o) => {
+      const mesh = o as Mesh
+
+      if (!mesh.isMesh || !mesh.material) return
+
+      const mat = mesh.material
+
+      if (!(mat instanceof Material) || mat.name === "pig") {
+        if (pig) {
+          pig.colorSpace = SRGBColorSpace
+          pig.flipY = false
+          pig.needsUpdate = true
+
+          if (mat instanceof MeshPhysicalMaterial) {
+            mat.map = pig
+            mat.needsUpdate = true
+          }
+        }
+      } else if (mat.name === "eye") {
+        if (eye) {
+          eye.colorSpace = SRGBColorSpace
+          eye.flipY = false
+          eye.needsUpdate = true
+
+          if (mat instanceof MeshStandardMaterial) {
+            mat.map = eye
+            mat.needsUpdate = true
+          }
+        }
+      } else if (mat.name === "pupil") {
+        if (pupil) {
+          pupil.colorSpace = SRGBColorSpace
+          pupil.flipY = false
+          pupil.needsUpdate = true
+
+          if (mat instanceof MeshStandardMaterial) {
+            mat.map = pupil
+            mat.needsUpdate = true
+          }
+        }
+      }
+    })
+  }, [clone, eye, pig, pupil])
 
   const { actions } = useAnimations(animations, clone)
 
@@ -29,8 +82,10 @@ export function PiggyBank({curAnim, ...props}: PiggyBankProps) {
     }
   }, [actions, curAnim])
 
-
   return <primitive object={clone} {...props} />
 }
 
+useKTX2.preload("/textures/pig.ktx2")
+useKTX2.preload("/textures/pig-eye.ktx2")
+useKTX2.preload("/textures/pig-pupil.ktx2")
 useGLTF.preload('/pig-transformed.glb')
